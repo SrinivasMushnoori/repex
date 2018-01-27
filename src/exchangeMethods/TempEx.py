28cbfd5aa03ee4e4968370ed1e0a5ba1c372fc3a
Srinivas Chakravarthy Mushnoori <scm177@two.radical-project.org>
2018-01-27 23:05:42 +0100

    add mdinfo file reader to exchange calculator

--- src/exchangeMethods/TempEx.py
+++ src/exchangeMethods/TempEx.py
@@ -3,8 +3,7 @@
 import os
 import sys
 import math
-import random
-
+#import numpy as np
 
 ####------------
 
@@ -18,25 +17,43 @@ def TemperatureExchange(Replicas):
     #random.shuffle(exchangeList)
     #####Read the mdinfo files######
 
+    Temp = 0.0
+    PotEng = 0.0
+    Replica_Temps = []
+    Replica_Energies = []
+    for n in range (Replicas):
+        f = open('mdinfo_{0}'.format(n))
+        lines = f.readlines()
+        #f.close
+        for i,j in enumerate(lines):
+            if "TEMP(K)" in lines[i]:
+                Temp = float(lines[i].split()[8])
+                
+                Replica_Temps.append(Temp)
+            elif "EPtot" in lines[i]:
+                PotEng = float(lines[i].split()[8])
+                Replica_Energies.append(PotEng)
+        f.close
+                
+
+    ##### Perform Exchange Computation
+
+             
 
 
 
-    #####
 
-
-
-
-    Col1 = exchangeList[:len(exchangeList)/2]
-    Col2= exchangeList[len(exchangeList)/2:]
+    #Col1 = exchangeList[:len(exchangeList)/2]
+    #Col2= exchangeList[len(exchangeList)/2:]
     ####exchangePairs = list(tuple)
-    exchangePairs = zip(Col1, Col2)
+    #exchangePairs = zip(Col1, Col2)
     ##Output exchange pairs to file
 
-    f = open('exchangePairs.txt', 'w')
-    for p in exchangePairs:
-        line = ' '.join(str(x) for x in p)
-        f.write(line + '\n')
-    f.close
-RandomExchange(Replicas)
+    #f = open('exchangePairs.txt', 'w')
+    #for p in exchangePairs:
+        #line = ' '.join(str(x) for x in p)
+        #f.write(line + '\n')
+    #f.close
+TemperatureExchange(Replicas)
 
                                                                 
