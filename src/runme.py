ec7a29c38db5d2f131847d3b9b6140a84c03e54a
Srinivas Chakravarthy Mushnoori <scm177@two.radical-project.org>
2018-02-05 21:59:46 +0100

    update main script

--- src/runme.py
+++ src/runme.py
@@ -6,35 +6,33 @@ import os
 # ------------------------------------------------------------------------------
 # Set default verbosity
 
-if os.environ.get('RADICAL_ENTK_VERBOSE') == None:
-    os.environ['RADICAL_ENTK_VERBOSE'] = 'INFO'
-
-#  Hard code the old defines/state names
-
-if os.environ.get('RP_ENABLE_OLD_DEFINES') == None:
-    os.environ['RP_ENABLE_OLD_DEFINES'] = 'True'
-
-if os.environ.get('RADICAL_PILOT_PROFILE') == None:
-    os.environ['RADICAL_PILOT_PROFILE'] = 'True'
-
-if os.environ.get('RADICAL_PILOT_DBURL') == None:
-    os.environ['RADICAL_PILOT_DBURL'] = "mongodb://smush:key1209@ds117848.mlab.com:17868/db_repex_1"
+os.environ['RADICAL_SAGA_VERBOSE'] = 'INFO'
+os.environ['RP_ENABLE_OLD_DEFINES'] = 'True'
+os.environ['RADICAL_ENMD_PROFILING'] = '1'
+os.environ['RADICAL_PILOT_PROFILE'] = 'True'
+os.environ['RADICAL_ENMD_PROFILE'] = 'True'
+os.environ['RADICAL_ENTK_VERBOSE'] = 'INFO'
+os.environ['RP_ENABLE_OLD_DEFINES'] = 'True'
+os.environ['SAGA_PTY_SSH_TIMEOUT'] = '2000'
+os.environ['RADICAL_VERBOSE'] = 'INFO'
+os.environ['RADICAL_PILOT_PROFILE'] = 'True'
+os.environ['RADICAL_PILOT_DBURL'] = "mongodb://smush:key1209@ds117848.mlab.com:17868/db_repex_1"
 
 #---------------------------------------#
 ## User Settings
 
 Replicas = 4
-Replica_Cores = 32
-Cycles = 2
-
+Replica_Cores = 20
+Cycles = 1
+#Resource = 'xsede.comet_ssh'
 #---------------------------------------#
 
 
 Book = []
 
-Pilot_Cores = Replicas * Replica_Cores    
+Pilot_Cores = (Replicas + 1) * Replica_Cores   
 
-def init_pipeline():
+def init_cycle():
 
     # Create Pipeline Obj
 
@@ -54,6 +52,7 @@ def init_pipeline():
         md_tsk = Task()
         #md_tsk.executable = ['/u/sciteam/mushnoor/amber/amber14/bin/sander.MPI']  #MD Engine, BW
         md_tsk.executable = ['/usr/local/packages/amber/16/INTEL-140-MVAPICH2-2.0/bin/pmemd.MPI'] #MD Engine, SuperMIC
+        #md_tsk.executable = ['/opt/amber/bin/pmemd.MPI']
         md_tsk.upload_input_data = ['inpcrd', 'prmtop', 'mdin_{0}'.format(n0)]
         #md_tsk.pre_exec = ['export AMBERHOME=$HOME/amber/amber14/']
         md_tsk.pre_exec = ['module load amber']    
@@ -79,10 +78,10 @@ def init_pipeline():
     for n1 in range (Replicas):
         ex_tsk.copy_input_data += ['%s/mdinfo_%s'%(d[n1],n1)]
     
-    ex_tsk.arguments = ['RandEx.py','{0}'.format(Replicas)]
+    ex_tsk.arguments = ['TempEx.py','{0}'.format(Replicas)]
     ex_tsk.cores = 1
     ex_tsk.mpi = False
-    ex_tsk.download_output_data = ['exchangePairs.txt']
+    ex_tsk.download_output_data = ['exchangePairs.dat']
     ex_stg.add_tasks(ex_tsk)
     task_uids.append(ex_tsk.uid)
     p.add_stages(ex_stg)
@@ -92,12 +91,12 @@ def init_pipeline():
     return p
 
 
-def general_pipeline(k):
+def cycle(k):
 
 
-    #read exchangePairs.txt
+    #read exchangePairs.dat
     #
-    with open("exchangePairs.txt","r") as f:
+    with open("exchangePairs.dat","r") as f:
         ExchangeArray = []
         for line in f:
             ExchangeArray.append(int(line.split(' ')[1]))
@@ -119,16 +118,17 @@ def general_pipeline(k):
     #Create MD task
     for n0 in range (Replicas):
         md_tsk = Task()
-        md_tsk.executable = ['/u/sciteam/mushnoor/amber/amber14/bin/sander.MPI']  #MD Engine
-
-        
+        #md_tsk.executable = ['/u/sciteam/mushnoor/amber/amber14/bin/sander.MPI']  #MD Engine, Blue Waters
+        md_tsk.executable = ['/usr/local/packages/amber/16/INTEL-140-MVAPICH2-2.0/bin/pmemd.MPI'] #MD Engine, SuperMIC 
+        #md_tsk.executable = ['/opt/amber/bin/pmemd.MPI']
         md_tsk.copy_input_data = ['%s/restrt > inpcrd'%(Book[k-1][ExchangeArray[n0]]),
                                   '%s/prmtop'%(Book[k-1][n0]),
                                   '%s/mdin_{0}'.format(n0)%(Book[k-1][n0])]
                                    ##Above: Copy from previous PIPELINE, make sure bookkeeping is correct
                                    
                               
-        md_tsk.pre_exec = ['export AMBERHOME=$HOME/amber/amber14/']
+        #md_tsk.pre_exec = ['export AMBERHOME=$HOME/amber/amber14/'] #Preexec, BLue Waters
+        md_tsk.pre_exec = ['module load amber']
         md_tsk.arguments = ['-O', '-i', 'mdin_{0}'.format(n0), '-p', 'prmtop', '-c', 'inpcrd', '-o', 'out']
         md_tsk.cores = Replica_Cores
         md_tsk.mpi = True
@@ -147,14 +147,14 @@ def general_pipeline(k):
 
     ex_tsk = Task()
     ex_tsk.executable = ['python']
-    ex_tsk.upload_input_data = ['exchangeMethods/RandEx.py']
+    ex_tsk.upload_input_data = ['exchangeMethods/TempEx.py']
     for n1 in range (Replicas):
         ex_tsk.copy_input_data += ['%s/mdinfo_%s'%(d[n1],n1)]
 
-    ex_tsk.arguments = ['RandEx.py','{0}'.format(Replicas)]
+    ex_tsk.arguments = ['TempEx.py','{0}'.format(Replicas)]
     ex_tsk.cores = 1
     ex_tsk.mpi = False
-    ex_tsk.download_output_data = ['exchangePairs.txt']
+    ex_tsk.download_output_data = ['exchangePairs.dat']
     ex_stg.add_tasks(ex_tsk)
     task_uids.append(ex_tsk.uid)
     p.add_stages(ex_stg)
@@ -169,44 +169,43 @@ if __name__ == '__main__':
 
     res_dict = {
                 #'resource': 'ncsa.bw_aprun',
-                'resource': xsede.supermic
+                'resource': 'xsede.supermic',
+                #'resource': 'xsede.comet',
                 'walltime': 30,
                 'cores': Pilot_Cores,
                 'access_schema': 'gsissh',
-                #'queue': 'debug',
-                'queue': 'workq'
-                'project': 'TG-MCB090174'
+                #'queue': 'compute',
+                'queue': 'workq',
+                'project': 'TG-MCB090174',
                 #'project': 'bamm',
                 }
 
     
 
-    #rman = ResourceManager(res_dict)
+    rman = ResourceManager(res_dict)
 
      # Create Application Manager
-    appman = AppManager(autoterminate=False)
+    appman = AppManager(autoterminate=False, port=33004)
 
      # Assign resource manager to the Application Manager
-    #appman.resource_manager = rman
-    p = general_pipeline(1)
-    appman.assign_workflow(set([p])
-    #p = init_pipeline()
+    appman.resource_manager = rman
+    p = init_cycle()
      # Assign the workflow as a set of Pipelines to the Application Manager
-    #appman.assign_workflow(set([p]))
+    appman.assign_workflow(set([p]))
 
      # Run the Application Manager
-    #appman.run()
+    appman.run()
 
-    #for k in range (Cycles):
-        #p = general_pipeline(k)
+    for k in range (Cycles):
+        p = cycle(k)
         #print p.uid
 
         # Assign the workflow as a set of Pipelines to the Application Manager
-        #appman.assign_workflow(set([p]))
+        appman.assign_workflow(set([p]))
 
         # Run the Application Manager
-        #appman.run()
+        appman.run()
 
 
 
-    #appman.resource_terminate()
+    appman.resource_terminate()
