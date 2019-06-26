43ec8fb013ed245a34cf6e8464376d1813b8cb3e
Andre Merzky <andre@merzky.net>
2019-06-26 21:49:34 +0200

    clean up tests
    Srinivas, you may want to start using an automated code checker to help with cleaner code ;-)

--- tests/test_async/test_async.py
+++ tests/test_async/test_async.py
@@ -6,116 +6,156 @@ import shutil
 # from repex import async_repex as async 
 from repex import dummy_exchange as async 
 
+
+# ------------------------------------------------------------------------------
+#
+def set_waitlist(exchange, rids):
+
+    exchange._waitlist = list()
+    for rid in rids:
+        exchange._waitlist.append(exchange._replicas[rid])
+
+
+# ------------------------------------------------------------------------------
+#
 def test_ReplicaExchange():
-    #exchange_size:  how big the exchange list needs to be
-    #                to move to the exchange phase
-    #window_size:    the width of the sliding window
-    replicaexchange = async.ReplicaExchange(ensemble_size = 16,
-                             exchange_size = 4,
-                             window_size   = 8,
-                             md_cycles    = 3, )
-                             #min_temp      = 300,
-                             #max_temp      = 320,
-                             #timesteps     = 500,
-                             #basename      = 'ace-ala', 
-                             #executable    = 'SANDER', 
-                             #cores         = 1)
+
+    # exchange_size:  how big the exchange list needs to be
+    #                 to move to the exchange phase
+    # window_size:    the width of the sliding window
+    exchange = async.ReplicaExchange(ensemble_size = 16,
+                                     exchange_size = 4,
+                                     window_size   = 8,
+                                     md_cycles     = 3,
+                                   # min_temp      = 300,
+                                   # max_temp      = 320,
+                                   # timesteps     = 500,
+                                   # basename      = 'ace-ala', 
+                                   # executable    = 'SANDER', 
+                                   # cores         = 1
+                                    )
+    assert len(exchange._replicas) == 16
 
 
-    assert len(replicaexchange._replicas) == 16
-    
-  # assign seven replicas to waitlist
-    replicaexchange._waitlist = [replicaexchange._replicas[0],replicaexchange._replicas[2], replicaexchange._replicas[4],replicaexchange._replicas[5],replicaexchange._replicas[12],replicaexchange._replicas[13],replicaexchange._replicas[15]]
-    old_replica_waitlist_length = len(replicaexchange._waitlist)
-    list_for_exchange = replicaexchange._find_exchange_list(4, 8, replicaexchange._replicas[0])
-    assert isinstance(list_for_exchange, list)
-    print [r.rid for r in list_for_exchange] 
-    assert(len(list_for_exchange) == 4) #, 'list: %s' % list_for_exchange
-    assert(len(replicaexchange._waitlist)==old_replica_waitlist_length-len(list_for_exchange))
-    assert(replicaexchange._replicas[0] in list_for_exchange)
-    for r in replicaexchange._waitlist:
-        assert(r not in list_for_exchange)
-    for r in list_for_exchange:
-        assert(r not in replicaexchange._waitlist)
+    # assign seven replicas to waitlist
+    set_waitlist(exchange, [0, 2, 4, 5, 12, 13, 15])
 
-  # assign seven replicas to waitlist with ending replicas fulfilling the criteria
-    replicaexchange._waitlist = [replicaexchange._replicas[0],replicaexchange._replicas[2], replicaexchange._replicas[4],replicaexchange._replicas[14],replicaexchange._replicas[12],replicaexchange._replicas[13],replicaexchange._replicas[15]]
-    old_replica_waitlist_length = len(replicaexchange._waitlist)
-    list_for_exchange = replicaexchange._find_exchange_list(4, 8, replicaexchange._replicas[12])
-    assert isinstance(list_for_exchange, list)
-    print [r.rid for r in list_for_exchange] 
-    assert(len(list_for_exchange) == 4) #, 'list: %s' % list_for_exchange
-    assert(len(replicaexchange._waitlist)==old_replica_waitlist_length-len(list_for_exchange))  
-    assert(replicaexchange._replicas[12] in list_for_exchange)
-    for r in replicaexchange._waitlist:
-        assert(r not in list_for_exchange)
-    for r in list_for_exchange:
-        assert(r not in replicaexchange._waitlist)
+    old_len = len(exchange._waitlist)
+    ex_list = exchange._find_exchange_list(4, 8, exchange._replicas[0])
+
+    assert(isinstance(ex_list, list))
+    assert(len(ex_list) == 4)
+    assert(len(exchange._waitlist) == old_len - len(ex_list))
+    assert(exchange._replicas[0] in ex_list)
+
+    for r in exchange._waitlist:
+        assert(r not in ex_list)
+
+    for r in ex_list:
+        assert(r not in exchange._waitlist)
 
 
-  # assign eight replicas to waitlist with two sets of replicas fulfilling the criteria
-    replicaexchange._waitlist = [replicaexchange._replicas[0],replicaexchange._replicas[2],replicaexchange._replicas[3], replicaexchange._replicas[4],replicaexchange._replicas[14],replicaexchange._replicas[12],replicaexchange._replicas[13],replicaexchange._replicas[15]]
-    old_replica_waitlist_length = len(replicaexchange._waitlist)
-    list_for_exchange = replicaexchange._find_exchange_list(4, 8, replicaexchange._replicas[2])
-    assert isinstance(list_for_exchange, list)
-    assert([r.rid for r in list_for_exchange] ==[0,2,3,4])
-    assert(len(list_for_exchange) == 4) #, 'list: %s' % list_for_exchange
-    assert(len(replicaexchange._waitlist)==old_replica_waitlist_length-len(list_for_exchange))  
-    for r in replicaexchange._waitlist:
-        assert(r not in list_for_exchange)
-    for r in list_for_exchange:
-        assert(r not in replicaexchange._waitlist)
+    # assign seven replicas to waitlist with ending replicas
+    # fulfilling the criteria
+    set_waitlist(exchange, [0, 4, 14, 12, 13, 15])
 
-    old_replica_waitlist_length = len(replicaexchange._waitlist)
-    list_for_exchange = replicaexchange._find_exchange_list(4, 8, replicaexchange._replicas[12])
-    assert isinstance(list_for_exchange, list)
-    assert([r.rid for r in list_for_exchange] ==[12,13,14,15]) 
-    assert(len(list_for_exchange) == 4) #, 'list: %s' % list_for_exchange
-    assert(len(replicaexchange._waitlist)==old_replica_waitlist_length-len(list_for_exchange)) 
-    for r in replicaexchange._waitlist:
-        assert(r not in list_for_exchange)
-    for r in list_for_exchange:
-        assert(r not in replicaexchange._waitlist)
+    old_len = len(exchange._waitlist)
+    ex_list = exchange._find_exchange_list(4, 8, exchange._replicas[12])
 
-  # assign four replicas to waitlist
-    replicaexchange._waitlist = [replicaexchange._replicas[0],replicaexchange._replicas[2], replicaexchange._replicas[4],replicaexchange._replicas[5]]
-    list_for_exchange = replicaexchange._find_exchange_list(4, 8, replicaexchange._replicas[0])
-    assert isinstance(list_for_exchange, list)
-    assert(len(list_for_exchange) == 4) #, 'list: %s' % list_for_exchange
-    assert(len(replicaexchange._waitlist)) == 0
-    for r in replicaexchange._waitlist:
-        assert(r not in list_for_exchange)
-    for r in list_for_exchange:
-        assert(r not in replicaexchange._waitlist)
+    assert(isinstance(ex_list, list))
+    assert(len(ex_list) == 4)
+    assert(len(exchange._waitlist) == old_len - len(ex_list))  
+    assert(exchange._replicas[12] in ex_list)
+
+    for r in exchange._waitlist:
+        assert(r not in ex_list)
+
+    for r in ex_list:
+        assert(r not in exchange._waitlist)
+
+
+    # assign eight replicas to waitlist with two sets of
+    # replicas fulfilling the criteria
+    set_waitlist(exchange, [0, 2, 3, 4, 14, 12, 13, 15])
+
+    old_len = len(exchange._waitlist)
+    ex_list = exchange._find_exchange_list(4, 8, exchange._replicas[2])
+
+    assert(isinstance(ex_list, list))
+    assert([r.rid for r in ex_list] == [0, 2, 3, 4])
+    assert(len(ex_list) == 4)
+    assert(len(exchange._waitlist) == old_len - len(ex_list))  
+
+    for r in exchange._waitlist:
+        assert(r not in ex_list)
+
+    for r in ex_list:
+        assert(r not in exchange._waitlist)
+
+    old_len = len(exchange._waitlist)
+    ex_list = exchange._find_exchange_list(4, 8, exchange._replicas[12])
+
+    assert(isinstance(ex_list, list))
+    assert([r.rid for r in ex_list] == [12, 13, 14, 15]) 
+    assert(len(ex_list) == 4)
+    assert(len(exchange._waitlist) == old_len - len(ex_list)) 
+
+    for r in exchange._waitlist:
+        assert(r not in ex_list)
+
+    for r in ex_list:
+        assert(r not in exchange._waitlist)
+
+
+    # assign four replicas to waitlist
+    set_waitlist(exchange, [0, 2, 4, 5])
+
+    ex_list = exchange._find_exchange_list(4, 8, exchange._replicas[0])
+
+    assert(isinstance(ex_list, list))
+    assert(len(ex_list) == 4)
+    assert(len(exchange._waitlist)) == 0
+
+    for r in exchange._waitlist:
+        assert(r not in ex_list)
+
+    for r in ex_list:
+        assert(r not in exchange._waitlist)
+
 
   # assign three replicas to waitlist
 
-    replicaexchange._waitlist = [replicaexchange._replicas[0],replicaexchange._replicas[2],replicaexchange._replicas[1]]
+    set_waitlist(exchange, [0, 2, 1])
 
-    list_for_exchange = replicaexchange._find_exchange_list(4, 8, replicaexchange._replicas[0])
+    ex_list = exchange._find_exchange_list(4, 8, exchange._replicas[0])
 
-    assert(list_for_exchange) == None #, 'list: %s' % list_for_exchange
+    assert((ex_list) is None)
+
 
   # assign two replicas to waitlist
 
-    replicaexchange._waitlist = [replicaexchange._replicas[0],replicaexchange._replicas[2]]
+    set_waitlist(exchange, [0, 2])
 
-    list_for_exchange = replicaexchange._find_exchange_list(4, 4, replicaexchange._replicas[0])
+    ex_list = exchange._find_exchange_list(4, 4, exchange._replicas[0])
 
-    assert(list_for_exchange) == None #, 'list: %s' % list_for_exchange
+    assert((ex_list) is None)
 
-    
 
+# ------------------------------------------------------------------------------
+#
 def cleanup():
+
     for filename in glob.glob("re.session*"):
         shutil.rmtree(filename) 
 
 
+# ------------------------------------------------------------------------------
+#
 if __name__ == '__main__':
 
-    test_Exchange()
-
+    test_ReplicaExchange()
     cleanup()
 
 
+# ------------------------------------------------------------------------------
 
