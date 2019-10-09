1f6126f0c7088adb6b59ba2b29a7013dc776b41a
Andre Merzky <andre@merzky.net>
2019-10-09 18:28:55 -0400

    snap

--- setup.py
+++ setup.py
@@ -225,7 +225,7 @@ setup_args = {
                                  'VERSION', 'CHANGES.md', 'SDIST', sdist_name]},
   # 'setup_requires'     : ['pytest-runner'],
     'install_requires'   : ['radical.utils',
-                            'radical.entk==0.7.17'],
+                           ],
     'tests_require'      : ['pytest',
                             'pylint',
                             'flake8',
